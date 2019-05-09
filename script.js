d954138d82c16c769b80ca8380482ce6407ebaf8
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-05-09 05:33:55 +0530

    Published

--- script.js
+++ script.js
@@ -455,8 +455,6 @@ function setXURL(node) {
 var initLoadDone = false;
 initLoad();
 function about_me() {
-	if(typeof gapi !== 'undefined')
-		gapi.plus.go('me_g-plus');
 	if(typeof twttr !== 'undefined')
 		twttr.widgets.load();
 }
